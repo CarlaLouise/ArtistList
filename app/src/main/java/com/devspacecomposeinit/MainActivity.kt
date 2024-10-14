5d966077c8f47ed599ce6a4189243ebae6732e95
CarlaLouise <148821309+CarlaLouise@users.noreply.github.com>
2024-10-13 21:22:22 -0300

    Criando Rom, Colunm e Card, mexendo com layout.

--- app/src/main/java/com/devspacecomposeinit/MainActivity.kt
+++ app/src/main/java/com/devspacecomposeinit/MainActivity.kt
@@ -3,18 +3,33 @@ package com.devspacecomposeinit
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.annotation.DrawableRes
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.devspacecomposeinit.ui.theme.ComposeInitTheme
 
 class MainActivity : ComponentActivity() {
@@ -27,42 +42,82 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    ArtistCard()
+                    val artist = Artist (
+                        name = "Leonardo da Vinci",
+                        lastSeenOnline = "3 minutes ago",
+                        image = R.drawable.ic_leonardo_da_vinci,
+                        art = R.drawable.ic_mona_lisa
+                    )
+                    ArtistCard(artist)
                 }
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
 
 @Composable
-fun ArtistCard() {
-    Column {
-        Text("Alfred Sisley")
-        Text("3 minutes ago")
-    }
+fun ArtistCard(artist: Artist) {
+    Column (
+        modifier = Modifier.padding(8.dp
+        )
+    ){
+    Row (verticalAlignment = Alignment.CenterVertically) {
+        Image(
+            modifier = Modifier
+                .size(60.dp)
+                .clip(CircleShape),
+            contentScale = ContentScale.FillWidth,
+            painter = painterResource(id = artist.image),
+            contentDescription = "Artist image"
+        )
+        Spacer(modifier = Modifier.size(16.dp))
+        Column {
+            Text(
+                text = artist.name,
+                fontSize = 18.sp,
+                fontWeight = FontWeight.SemiBold
+            )
+            Text(
+                text = artist.lastSeenOnline,
+                color = Color.Gray
+            )
+         }
+      }
+      Card (
+          modifier = Modifier
+              .padding(8.dp),
+          elevation = CardDefaults.cardElevation(defaultElevation = 4.dp)
+      ){
+          Image(
+              modifier = Modifier.fillMaxWidth()
+                  .height(200.dp),
+              contentScale = ContentScale.Crop,
+              painter = painterResource(id =artist.art),
+              contentDescription = "Artist art")
+      }
+   }
 }
+
+data class Artist(
+    val name: String,
+    val lastSeenOnline: String,
+    @DrawableRes val image: Int,
+    @DrawableRes val art: Int,
+)
 
 @Preview(showBackground = true)
 @Composable
 fun ArtistCardPreview() {
 
     ComposeInitTheme {
-        ArtistCard()
+        val artist = Artist (
+            name = "Leonado da Vinci",
+            lastSeenOnline = "3 minutes ago",
+            image = R.drawable.ic_leonardo_da_vinci,
+            art = R.drawable.ic_mona_lisa,
+        )
+        ArtistCard(artist)
     }
 }
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    ComposeInitTheme {
-        Greeting("Android")
-    }
-}
